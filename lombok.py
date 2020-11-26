2eaa8bf2252fe2b969f91ebb6970f666e5cc36f4
cyberlombok <72059070+cyberlombok@users.noreply.github.com>
2020-11-26 11:15:27 +0700

    Add files via upload

---
+++ lombok.py
@@ -0,0 +1,74 @@
+#ToolsByKhairulFatihin
+#MANTANPRAWAN.$
+#Boleh Record Asal Cantumkan Nama Author
+#Hargai Karya Saya :)
+
+import os,sys,time
+
+
+def bersih():
+     os.system("clear")
+
+def main():
+    time.sleep(1)
+    bersih()
+    time.sleep(1)
+    print ('\33[1;33m                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            Hello \33[31;1mBangsat \33[0;36m:)  ')
+    time.sleep(1)
+    bersih()
+    print ('\33[32;1m')
+    os.system ('figlet Tools Crack FB')
+    print'\33[36;1m+\33[1;33m===========================================\33[36;1m+'
+    print'\33[36;1mAuthor       \33[32;1m: \33[37;1mKHAIRUL FATIHIN '
+    print'\33[36;1mFacebook  \33[32;1m: \33[37;1mhttps://www.facebook.com/khairul.fatihin557 '
+    print'\33[36;1m+\33[1;33m===========================================\33[36;1m+'
+    print'\33[32;1m============  \33[37;1mM E N U \33[32;1m============'
+    print'\33[32;1m[\33[37;1m1\33[32;1m] \33[39;1mHack Fb Login Token '
+    print'\33[32;1m[\33[37;1m2\33[32;1m] \33[39;1mHack Fb Login Cokie '
+    print'\33[32;1m[\33[37;1m3\33[32;1m] \33[39;1mUbah Cookies Jadi Token '
+    print'\33[32;1m[\33[31;1m0\33[32;1m] \33[31;1mExit '
+    gans = raw_input ('\33[36;1m==> ')
+    if gans in ['1']:
+        time.sleep(1)
+        os.system('pkg install python2')
+        os.system('pip2 install tqdm')
+        os.system('pip2 install requests')
+        os.system('pip2 install mechanize')
+        os.system ('git clone https://github.com/MantanPrawan/khairulfatihin')
+        bersih()
+	time.sleep(2)
+	print'\33[1;33mBERHASIL DIISNSTALL.......'
+	bersih()
+        os.system('cd khairulfatihin;python2 khairulfatihin.py')
+    if gans in ['2']:
+        time.sleep(1)
+        os.system('pkg update && pkg upgrade')
+        os.system('pkg install python git')
+        os.system('pip install requests bs4 futures')
+        os.system('git clone https://github.com/cyberlombok/cyberlombok')
+        bersih()
+	time.sleep(2)
+        print'\33[1;33mBERHASIL DIISNSTALL.......'
+	bersih()
+        os.system('cd cyberlombok;python cyberlombok.py')
+	sys.exit
+    if gans in ['3']:
+	os.system('pkg update && pkg upgrade')
+	os.system('pkg install python git')
+	os.system('pip install requests')
+	os.system('git clone https://github.com/dz-id/fb_get_token_from_cookie')
+	bersih()
+	time.sleep(2)
+	print'\33[1;33mBERHASIL DIISNSTALL.......'
+        bersih()
+	os.system('cd fb_get_token_from_cookie;python main.py')
+    if gans in ['0']:
+        time.sleep(2)
+        exit()
+    else:
+        time.sleep(1)
+        print ' Pilih Yang Bener Goblok . . .'
+        time.sleep(1)
+        main()
+
+main()
